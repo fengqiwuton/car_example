4558b5ead8b4d755612bfc8a67c1297b3eb2b596
fengqiwuton <3783725038@qq.com>
2026-07-05 20:26:42 +0800

    bottom

--- user/track_control.h
+++ user/track_control.h
@@ -5,7 +5,9 @@
 
 typedef struct
 {
+	/* Raw digital data from the module. */
 	uint8_t raw;
+	/* Active black-line bits after polarity conversion. */
 	uint8_t bits;
 	uint8_t active_count;
 	uint8_t no_frame_count;
@@ -18,14 +20,30 @@ typedef struct
 	uint16_t analog[8];
 } Track_Info_t;
 
+typedef struct
+{
+	/* Kp * 100, for example 78 means 0.78. */
+	int kp_x100;
+	/* Kd * 100, for example 28 means 0.28. */
+	int kd_x100;
+} Track_PD_t;
+
+/* UART tracking module */
 void track_control_init(void);
 void track_control_request_data(void);
 void track_uart_rx(uint8_t data);
+
+/* Line-following control */
 void track_follow_update(void);
 void track_car_drive(int left_speed, int right_speed);
 void track_car_stop(void);
 uint8_t track_has_line(void);
 uint8_t track_line_lost(void);
 Track_Info_t track_get_info(void);
+
+/* Runtime PD tuning. Values are stored as Kp/Kd * 100 to avoid float UI code. */
+Track_PD_t track_pd_get(void);
+void track_pd_set(int kp_x100, int kd_x100);
+void track_pd_adjust(int kp_delta_x100, int kd_delta_x100);
 
 #endif
